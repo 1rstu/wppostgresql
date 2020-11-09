9a0e2bf453e19e8e0e85582e4de9fbaba80d6b60
1rstu <59177653+1rstu@users.noreply.github.com>
2020-11-09 18:25:24 +0700

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'd7pg77skiq7l4n' );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'omszcfmkyizpur' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', '9c967fe9aec8f57399553e798eb63683f3e203a85ceefb6fae315cbc4d95191e' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'ec2-54-157-4-216.compute-1.amazonaws.com:5432/d7pg77skiq7l4n' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
